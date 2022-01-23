5345148480fb89d0ee54afdd1a767344083b0f67
nmarrocchi <nmarrocchi@la-providence.net>
2022-01-23 17:00:58 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="style/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Centuries</title>
+</head>
+
+<body>
+
+  <div id="container">
+    <div id="banner">
+      <img src="img/banner.png" alt="Centuries banner">
+    </div>
+
+    <div id="nav">
+      <ul>
+        <a href=""><li>Home</li></a>
+        <a href=""><li>Apply on Discord</li></a>
+        <a href=""><li>Club Policies</li></a>
+        <a href=""><li>About Us</li></a>
+        <a href=""><li>Helpfull Links</li></a>
+      </ul>
+    </div>
+    
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+  </div>
+  
+</body>
+</html>
